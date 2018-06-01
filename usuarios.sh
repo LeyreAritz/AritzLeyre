06bd5d2832a0909ced7f68bcbabdbb0f51d5232b
Leyre Aritz <leire.lago@elorrieta-errekamari.com>
2018-06-01 13:36:54 +0200

    Cambios en el menu agregar usuario a un grupo que existe en usuarios.sh

--- usuarios.sh
+++ usuarios.sh
@@ -3,7 +3,7 @@
 echo "1. Crear un usuario nuevo"
 echo "2. Añadir una contraseña a un usuario existente"
 echo "3. Crear un grupo nuevo"
-echo "4. Añadir usuario a un grupo existente"
+echo "4. Añadir usuario a un grupo que ya existe"
 echo "5. Salir del programa"
 
 read opcion
